11a687680d8416ccbe26b296549729e66a0a9c3f
Krishaa1310 <Krishaa1310@users.noreply.github.com>
2026-03-17 10:21:35 +0530

    Fix base path for Vite build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/SafeBillTracker/",
+  base: './',
   plugins: [react(), tailwindcss()],
 })
